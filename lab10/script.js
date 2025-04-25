e523238ddfdd9cf27a2cdf01389edfcf03e1b22e
KidawR <vadimser2002@mail.ru>
2025-04-25 14:23:12 +0300

    fix

--- lab10/script.js
+++ lab10/script.js
@@ -6,13 +6,15 @@ export const options = {
     duration: '60s', // общее время теста
 };
 
+const requestTimeout = '360s'; // таймаут 6 минут
+
 export default function () {
     const chance = Math.random();
 
     if (chance < 0.05) { // 5% запросов - создание клиента
         const payload = JSON.stringify({
-            name: 'Client ${__VU}_${__ITER}',
-            email: '+7${Math.floor(9000000000 + Math.random() * 100000000)}'
+            name: `Client ${__VU}_${__ITER}`,
+            email: `+7${Math.floor(9000000000 + Math.random() * 100000000)}`
         });
 
         const headers = {
@@ -20,14 +22,24 @@ export default function () {
             'Content-Type': 'application/json'
         };
 
-        const res = http.post('http://192.168.1.232:8080/viewers', payload, { headers });
+        const res = http.post(
+            'http://192.168.1.232:8080/viewers',
+            payload,
+            {
+                headers,
+                timeout: requestTimeout,
+            }
+        );
 
         check(res, {
             'client created successfully': (r) => r.status === 200 || r.status === 201,
         });
 
     } else { // 95% запросов - выполнение доп задания
-        const res = http.get('http://192.168.1.232:8080/tickets/stats');
+        const res = http.get(
+            'http://192.168.1.232:8081/stats/',
+            { timeout: requestTimeout }
+        );
 
         check(res, {
             'clients retrieved successfully': (r) => r.status === 200,
